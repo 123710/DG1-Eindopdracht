3bec3a3cf634b61aff2a25abd0bcb18be3d90f1a
Bashar <123710@student.drenthecollege.nl>
2019-02-25 10:29:03 +0100

    Bashar toegevoegd

---
+++ DeEerste.php
@@ -0,0 +1,9 @@
+
+<html>
+<head>
+</head>
+<body>
+  <h1>hallo from the first page
+  </h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 <body>
   <h1>Eerste pagina</h1>
   <h2>test</h2>
+  <h3>Bashar</h3>
 </body>
 </html>
